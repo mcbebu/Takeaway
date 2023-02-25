b430c5664a503ee1819899f1dcbe80b371d77c0d
PeterzGitAcc <peter.chang.2020@smu.edu.sg>
2023-02-25 23:52:21 +0800

    Revert "updates to live chat"
    This reverts commit e6ec5e0f1926d1bb23bdddd4f81be759ecdd0090.

--- frontend/ninjawalker/src/views/LiveChatView.vue
+++ frontend/ninjawalker/src/views/LiveChatView.vue
@@ -1,18 +1,20 @@
 <template>
 <h1>Live Chat</h1>
 <div class="container-fluid">
-
-
-      </div>
-
+<div id="message-container">
+    <form id ="send-container">
+        <input type="text" id="message-input">
+        <button type="submit" id="send-button">Send</button>
+    </form>
+</div>
+</div>
 </template>
 
 <script>
 
 export default {
-name: 'LiveChatView',
+name: 'LiveChatView'
 }
-
 </script>
 
 <style>
