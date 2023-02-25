e6ec5e0f1926d1bb23bdddd4f81be759ecdd0090
PeterzGitAcc <peter.chang.2020@smu.edu.sg>
2023-02-25 20:32:46 +0800

    updates to live chat

--- frontend/ninjawalker/src/views/LiveChatView.vue
+++ frontend/ninjawalker/src/views/LiveChatView.vue
@@ -1,20 +1,18 @@
 <template>
 <h1>Live Chat</h1>
 <div class="container-fluid">
-<div id="message-container">
-    <form id ="send-container">
-        <input type="text" id="message-input">
-        <button type="submit" id="send-button">Send</button>
-    </form>
-</div>
-</div>
+
+
+      </div>
+
 </template>
 
 <script>
 
 export default {
-name: 'LiveChatView'
+name: 'LiveChatView',
 }
+
 </script>
 
 <style>
